fa08d59bfe7097181bc959b6e59c605b22d070cc
SYNAPSE\mayank.bahuguna2 <mayank.bahuguna.ext@thesiscloud.com>
2024-04-10 10:17:14 +0530

    work on customizing theme

--- src/theme/customTheme.tsx
+++ src/theme/customTheme.tsx
@@ -19,6 +19,27 @@ const theme = createTheme({
     status: {
         danger: orange[500],
     },
+    palette: {
+        secondary: {
+            main: "#E33E7F",
+        },
+        primary: {
+            main: "#E33E7F",
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: "blue", // Change the background color of all buttons
+                },
+                contained: {
+                    backgroundColor: "red", // Change the background color of contained buttons
+                },
+                // You can add specific styles for other button variants like outlined, text, etc.
+            },
+        },
+    },
 });
 
 export default theme;
